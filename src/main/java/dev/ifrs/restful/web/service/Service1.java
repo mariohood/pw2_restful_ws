dbea2c8972d77b13d311c19964bbad41585ed5fa
mariohood <manhattan199@hotmail.com>
2022-05-31 14:09:43 -0300

    service2

---
+++ src/main/java/dev/ifrs/restful/web/service/Service1.java
@@ -0,0 +1,20 @@
+package dev.ifrs.restful.web.service;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ *
+ */
+@Path("/api/{nos}")
+@Singleton
+public class Service1 {
+
+    @GET
+    public double conversao(@PathParam("nos") double nos) {
+
+        return nos*1.852;
+    }
+}
