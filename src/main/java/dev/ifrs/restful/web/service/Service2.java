29b3a69e2a32b8206c0f40ccc42520eb77bb440f
mariohood <manhattan199@hotmail.com>
2022-05-31 14:16:20 -0300

    service2 produce json

--- src/main/java/dev/ifrs/restful/web/service/Service2.java
+++ src/main/java/dev/ifrs/restful/web/service/Service2.java
@@ -4,15 +4,18 @@ import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  */
-@Path("/api/{nos}")
+@Path("/api2/{nos}")
 @Singleton
 public class Service2 {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public double conversao(@PathParam("nos") double nos) {
 
         return nos*1.852;
